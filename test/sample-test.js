3cd6d759d839f9e04cff4f4f6a4dd967fb6312b4
Jared Crace <jaredcrace@Jareds-MBP.lan>
2022-03-08 17:30:06 -0600

    changing name in test

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,10 +3,10 @@ const { expect, assert } = require("chai");
 // to run do the following:
 // > npx hardhat test
 
-describe("ModifyVariable", function () {
+describe("TicketMundo", function () {
   it("should change x to 1337", async function () {
-    const ModifyVariable = await ethers.getContractFactory("ModifyVariable");
-    const contract = await ModifyVariable.deploy(10);
+    const TicketMundo = await ethers.getContractFactory("TicketMundo");
+    const contract = await TicketMundo.deploy(10);
     await contract.deployed();
 
     // modify x from 10 to 1337 via this function!
