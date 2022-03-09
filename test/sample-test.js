6c75bfb2057500c5be693f757c3a22439f4ec595
Jared Crace <jaredcrace@Jareds-MBP.lan>
2022-03-08 18:15:18 -0600

    ticket generation working

--- test/sample-test.js
+++ test/sample-test.js
@@ -6,11 +6,22 @@ const { expect, assert } = require("chai");
 describe("TicketMundo", function () {
   it("should change x to 1337", async function () {
     const TicketMundo = await ethers.getContractFactory("TicketMundo");
-    const contract = await TicketMundo.deploy(10);
+
+    // generate some random numbers for tickets
+    let ticketArray = [];
+    for(var i=0; i<10; i++) {
+      ticketArray.push(Math.floor(Math.random() * 999999999));
+    }
+
+    console.log(ticketArray);
+
+    const contract = await TicketMundo.deploy(10, ticketArray);
     await contract.deployed();
+
 
     // modify x from 10 to 1337 via this function!
     await contract.modifyToLeet();
+
     // getter for state variable x
     const newX = await contract.x();
     assert.equal(newX.toNumber(), 1337);
